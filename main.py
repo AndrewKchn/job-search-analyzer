0834f530ab00f83f491b606273ffc3063db8e590
Andrew <andrewkchn@gmail.com>
2026-05-08 00:49:37 +0200

    feat: added storing data to csv file

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
 from clients.fetcher import Fetcher
+from database.file_repository import CsvRepository
 
 fetcher = Fetcher("https://www.arbeitnow.com/api/job-board-api")
 
-fetcher.fetch_page()
+jobs = fetcher.fetch_page()
+
+repository = CsvRepository("local_storage/jobs.csv")
+repository.save_jobs(jobs)
