a4956e0cb396c98e05753f4268bbaa496881056a
Andrew <andrewkchn@gmail.com>
2026-05-13 15:20:17 +0200

    test: add sync_service tests

--- services/sync_service.py
+++ services/sync_service.py
@@ -5,7 +5,7 @@ from loguru import logger
 
 from clients.api_client import ArbeitnowClient
 from config import MIN_SLEEP_BETWEEN_REQUESTS, MAX_SLEEP_BETWEEN_REQUESTS
-from database.file_repository import CsvRepository
+from repository.file_repository import CsvRepository
 from models.job_dto import JobDTO
 
 
@@ -15,7 +15,7 @@ class SyncService:
         self.client = client
         self.repository = repository
 
-    def _sync_jobs_form_page(self, page_number: int = 1):
+    def _sync_jobs_from_page(self, page_number: int = 1):
         response = self.client.get_jobs_from_page(page_number)
 
         job_list = response['data']
@@ -23,16 +23,16 @@ class SyncService:
         self.repository.save_jobs(job_list_dto)
         return response['links']['next']
 
-    def __waiting(self):
+    def _waiting(self):
         delay = random.uniform(MIN_SLEEP_BETWEEN_REQUESTS, MAX_SLEEP_BETWEEN_REQUESTS)
         logger.debug(f"Waiting {delay:.2f} seconds...'")
         time.sleep(delay)
 
-    def sync_jobs_form_all_pages(self):
+    def sync_jobs_from_all_pages(self):
         page_number = 1
         while True:
-            self.__waiting()
-            has_next_page = self._sync_jobs_form_page(page_number)
+            self._waiting()
+            has_next_page = self._sync_jobs_from_page(page_number)
             if not has_next_page:
                 logger.success("All jobs have been added to the file")
                 break
