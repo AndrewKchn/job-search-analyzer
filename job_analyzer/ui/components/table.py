e6b1207bebf39ba55797c333105d60be6e16fe77
AndrewKchn <59029249+AndrewKchn@users.noreply.github.com>
2026-05-30 07:48:48 +0200

    refactor: add description to table (#19)

--- job_analyzer/ui/components/table.py
+++ job_analyzer/ui/components/table.py
@@ -1,8 +1,19 @@
 import streamlit as st
 
+
 def render_table(df):
-    st.subheader("Jobs")
+    st.subheader("Stored Vacancies")
 
     st.dataframe(
-        df[["title", "company_name", "location", "remote", "created_at"]]
+        df[["title", "company_name", "location", "description", "remote", "created_at"]],
+        column_config={
+            "title": "Title",
+            "company_name": "Company",
+            "location": "Location",
+            "description": "Description",
+            "remote": "Remote",
+            "tags": "Tags",
+            "created_at": "Creation Date"
+        },
+        hide_index=True
     )
